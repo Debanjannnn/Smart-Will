943c2da56ea2d582070da6e76c9c13a107cd67bb
Debanjan Mondal <debanjanmondalk2005@gmail.com>
2025-01-03 20:52:25 +0530

    Update BalanceDisplay.jsx

--- components/BalanceDisplay.jsx
+++ components/BalanceDisplay.jsx
@@ -4,7 +4,7 @@ export default function BalanceDisplay({ balance }) {
         <div className="flex justify-center items-center mt-4">
                <div className=" p-6 rounded-xl shadow-lg flex items-center space-x-6">
                <p className="font-['Press_Start_2P'] text-white text-lg md:text-2xl">
-                  We have kept {balance}
+                   We have kept {balance} EDU, save with us
                  </p>
           <img
             src="https://media.tenor.com/o_E6C9Jez2YAAAAj/chest-minecraft.gif"
